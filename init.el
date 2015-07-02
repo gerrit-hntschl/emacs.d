44ec383dfe898b20b331fc7626fb36ed5e8c7faf
Gerrit Hentschel <gerrit.hentschel@gmail.com>
2015-07-02 22:02:01 +0200

    switch to melpa; delete obsolete packages

--- init.el
+++ init.el
@@ -1,12 +1,37 @@
+(require 'cl)
 (require 'package)
 
-(add-to-list
- 'package-archives
- '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;(add-to-list
+;; 'package-archives
+;; '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
 
-(require 'cl)
+(defvar prelude-packages
+  '(clojure-mode gist haml-mode exec-path-from-shell
+                   magit markdown-mode paredit 
+                   yaml-mode zenburn-theme)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'prelude-packages)
 
 (add-to-list 'load-path "~/.emacs.d/lib")
 
--- my/magit.el
+++ my/magit.el
@@ -1,3 +1,5 @@
+(require 'exec-path-from-shell)
+
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
